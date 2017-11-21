e43513e6c996b505387eab42b45511cf4fe955d9
Sandor Torok <sandor.torok@ft.com>
2017-11-21 16:26:29 +0200

    Increased neo connection timeout for k8s

--- main.go
+++ main.go
@@ -78,7 +78,7 @@ func main() {
 	app.Action = func() {
 		log.WithField("event", "service_started").WithField("service_name", *appName).Info("Service started")
 		conf := neoutils.DefaultConnectionConfig()
-		conf.HTTPClient.Timeout = 5 * time.Minute
+		conf.HTTPClient.Timeout = 8 * time.Minute
 		neoConn, err := neoutils.Connect(*neoURL, conf)
 
 		if err != nil {
