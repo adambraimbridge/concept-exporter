edd8221b5bfd15a787b30adb52d4c8c395bc94e2
marina <marina.chompalova@ft.com>
2020-03-24 15:45:12 +0200

    Fix logger messages

--- concept/inquirer.go
+++ concept/inquirer.go
@@ -59,23 +59,25 @@ func (n *NeoInquirer) Inquire(candidates []string, tid string) []*Worker {
 		workers = append(workers, worker)
 	}
 	go func() {
-		n.Log.WithField("transaction_id", tid).Infof("Starting reading concepts from Neo: %v", candidates)
+		logEntry := n.Log.WithTransactionID(tid)
+		logEntry.Infof("Starting reading concepts from Neo: %v", candidates)
 		for _, worker := range workers {
 			count, found, err := n.Neo.Read(worker.ConceptType, worker.ConceptCh)
 			if err != nil {
-				n.Log.WithField("transaction_id", tid).Errorf("Error by reading %v concept type from Neo: %+v", worker.ConceptType, err)
+				logEntry.WithError(err).Errorf("error by reading %v concept type from Neo", worker.ConceptType)
 				worker.Errch <- err
 				continue
 			}
 			if !found {
-				n.Log.WithField("transaction_id", tid).Errorf("Reading %v concept type from Neo returned empty result", worker.ConceptType)
-				worker.Errch <- fmt.Errorf("Reading %v concept type from Neo returned empty result", worker.ConceptType)
+				err = fmt.Errorf("reading %v concept type from Neo returned empty result", worker.ConceptType)
+				logEntry.Error(err)
+				worker.Errch <- err
 				continue
 			}
-			n.Log.WithField("transaction_id", tid).Infof("Found %v entries for %v concept", count, worker.ConceptType)
+			logEntry.Infof("Found %v entries for %v concept", count, worker.ConceptType)
 			worker.setCount(count)
 		}
-		n.Log.WithField("transaction_id", tid).Info("Finished Neo read")
+		logEntry.Info("Finished Neo read")
 	}()
 	return workers
 }
